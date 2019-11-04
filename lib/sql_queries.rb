d49a3e54114c48b939dd87b6827d056b706b296e
Jonathan Norman <jnorman2123@gmail.com>
2019-11-04 19:17:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,10 +19,10 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) AS num 
-    FROM bears 
-    GROUP BY color 
-    ORDER BY num DESC 
+  "SELECT color, COUNT(color) AS num
+    FROM bears
+    GROUP BY color
+    ORDER BY num DESC
     LIMIT 2;"
 end
 
