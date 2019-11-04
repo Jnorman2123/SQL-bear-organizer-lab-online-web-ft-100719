063508242ea45ac91b39f921d6b4cf2efbaf1754
Jonathan Norman <jnorman2123@gmail.com>
2019-11-04 18:15:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, null, null, null, null, null)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, null, null, null, null, null);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, null, null, null, null, null);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, null, null, null, null, null);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, null, null, null, null, null);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, null, null, null, null, null);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, null, null, null, null, null);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, null, null, null, null, null);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, null, null, null, null, null);
