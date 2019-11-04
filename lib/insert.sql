3e53f15397c627214d1ce81e2372add6e55a3fda
Jonathan Norman <jnorman2123@gmail.com>
2019-11-04 18:17:53 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("John", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bob", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bill", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Jack", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Jill", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rachel", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Gerry", NULL, NULL, NULL, NULL, NULL);
