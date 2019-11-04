ea7710834203ef5c3057126c67f2c8a9fd9adc9b
Jonathan Norman <jnorman2123@gmail.com>
2019-11-04 19:18:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
     FROM bears
     GROUP BY color
     ORDER BY num DESC
-    LIMIT 2;"
+    LIMIT 3;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
